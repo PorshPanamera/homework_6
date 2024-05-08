77e599eed987ab7adaefb3ced3a087a139983470
PorshPanamera <evaivaniseva0@gmail.com>
2024-05-08 13:13:06 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var num1 = parseFloat(
+  prompt("Please,enter numbers to calculate their arithmetic average:", "")
+);
+var num2 = parseFloat(prompt("Enter second number:", ""));
+var num3 = parseFloat(prompt("Enter third number:", ""));
+let average = (num1 + num2 + num3) / 3;
+alert("Arithmetic average: " + average);
